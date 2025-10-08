b04aabf122847a5972bbe6c858878a3d87364429
Luk566 <luk@bussat.eu>
2025-10-08 10:08:11 +0200

    First uploaded version

---
+++ unearthed.py
@@ -0,0 +1,35 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop,Axis
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+################################################################################
+# Funksjoner
+
+
+################################################################################
+# Definisjoner
+hub = PrimeHub(front_side=-Axis.Z,top_side=-Axis.Y)
+farge_sensor = ColorSensor(Port.A)
+
+left_motor  = Motor(Port.F,Direction.COUNTERCLOCKWISE)
+right_motor  = Motor(Port.B)
+front_motor = Motor(Port.D)
+back_motor = Motor(Port.C)
+drive_base  = DriveBase(left_motor, right_motor, wheel_diameter=88, axle_track=112)
+################################################################################
+
+while True:
+    if Button.LEFT in hub.buttons.pressed():
+        drive_base.use_gyro(False)
+        drive_base.straight(10)
+        drive_base.use_gyro(True)
+        drive_base.settings(1000, 1000, 1100, 950)
+        drive_base.curve(735,45,Stop.COAST_SMART)
+        drive_base.turn(-90,Stop.COAST_SMART)
+        wait(1000)
+        drive_base.settings(200, 1000, 1100, 950)
+        drive_base.straight(250,Stop.COAST_SMART)
+
+        drive_base.settings(1000, 1000, 1100, 950)
+        drive_base.straight(-80,Stop.COAST_SMART)
